ee9480630daa548f3a4e07378ce4c4dc23658799
Anton Timmermans <anton@yoast.com>
2015-09-21 09:21:02 +0200

    Allow the search URL to be filtered

--- html_includes/partials/search.php
+++ html_includes/partials/search.php
@@ -1,13 +1,15 @@
 <?php
 namespace Yoast\YoastCom\Theme;
+
+$search_url = apply_filters( 'yoastcom_search_url', home_url() );
 ?>
 <?php if ( 'mobile' === $template_args['type'] ) : ?>
-	<form action="<?php echo esc_attr( home_url() ); ?>" class="hide-on-desktop mobile-search" id="mobile-search" data-mobile-search>
+	<form action="<?php echo esc_attr( $search_url ); ?>" class="hide-on-desktop mobile-search" id="mobile-search" data-mobile-search>
 		<input type="search" name="s" autofocus placeholder="Search">
 		<button>Search &raquo;</button>
 	</form>
 <?php elseif ( 'desktop' === $template_args['type'] ) : ?>
-	<form action="<?php echo esc_attr( home_url() ); ?>">
+	<form action="<?php echo esc_attr( $search_url ); ?>">
 		<input type="search" name="s" class="size-m" placeholder="Search">
 		<button class="button--naked">
 			<span class="visuallyhidden focusable">Search</span>
