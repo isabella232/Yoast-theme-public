1b501408bd56aae1848da9c6e5072f4742c21d74
Anton Timmermans <anton@yoast.com>
2015-09-28 13:46:30 +0200

    Add the email template to the theme

---
+++ html_includes/emails/academy-invite.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Yoast\YoastCom\Theme;
+
+?>
+Thank you for buying a course in Yoast Academy
+
+<a href="<?php echo esc_url( $template_args['url'] ); ?>">Login or register for an account here.</a>
+
+If you cannot click on the link, paste this url in the browser: <?php echo esc_url( $template_args['url'] ); ?>
+
+
+Greetings,
+Team Yoast
+
