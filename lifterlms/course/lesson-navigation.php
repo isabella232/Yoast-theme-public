05950b825cb7047fbcf142616a5faa3794656482
Anton Timmermans <anton@yoast.com>
2015-10-01 08:49:16 +0200

    Improve lesson navigation

--- lifterlms/course/lesson-navigation.php
+++ lifterlms/course/lesson-navigation.php
@@ -2,53 +2,35 @@
 
 namespace Yoast\YoastCom\Theme;
 
+use Yoast\YoastCom\Academy\Lesson;
+
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-/* @var \LLMS_Lesson $lesson */
+/* @var Lesson $lesson */
 $lesson = \Yoast\YoastCom\Academy\get_lesson();
+
+$next_lesson_id = $lesson->get_global_next_lesson();
+$previous_lesson_id = $lesson->get_global_previous_lesson();
+$parent_course_id = $lesson->get_parent_course();
 ?>
 
 <nav class="llms-course-navigation">
 
-	<?php
-	if ( $lesson->get_previous_lesson() ) {
-		$previous_lesson_id = $lesson->get_previous_lesson();
-		$previous_lesson_link = get_permalink ( $previous_lesson_id );
-		?>
+	<a class="button dimmed left" href="<?php echo esc_url( get_permalink( $parent_course_id ) ); ?>">
+		<?php _e( 'Back to Course', 'yoastcom' ); ?>
+	</a>
 
-		<div class="llms-lesson-preview prev-lesson previous">
-			<a class="llms-lesson-link" href="<?php echo $previous_lesson_link; ?>" alt="<?php echo __('Previous Lesson', 'lifterlms'); ?>">
-				<?php _e( 'Previous Lesson', 'yoast-academy' ); ?>
-			</a>
-		</div>
+	<?php if ( $previous_lesson_id ) : ?>
+		<a class="button previous-lesson dimmed left" href="<?php echo esc_url( get_permalink( $previous_lesson_id ) ); ?>">
+			<?php _e( 'Previous Lesson', 'yoastcom' ); ?>
+		</a>
+	<?php endif; ?>
 
-	<?php }
-
-	if ( !$lesson->get_previous_lesson() || !$lesson->get_next_lesson() ) {
-		$parent_style = $lesson->get_next_lesson() ? 'llms-lesson-preview prev-lesson previous' : 'llms-lesson-preview next-lesson next';
-		$parent_course_id = $lesson->get_parent_course();
-		$parent_course_link = get_permalink ( $parent_course_id );
-
-		?>
-		<div class="llms-lesson-preview <?php echo $parent_style; ?>">
-			<a class="llms-lesson-link" href="<?php echo $parent_course_link; ?>" alt="<?php echo __('Back to Course', 'lifterlms'); ?>">
-				<?php _e( 'Back to Course', 'yoast-academy' ); ?>
-			</a>
-		</div>
-	<?php }
-
-	if ( $lesson->get_next_lesson() ) {
-		$next_lesson_id = $lesson->get_next_lesson();
-		$next_lesson_link = get_permalink ( $next_lesson_id );
-		?>
-
-		<div class="llms-lesson-preview next-lesson next">
-			<a class="llms-lesson-link" href="<?php echo $next_lesson_link; ?>" alt="<?php echo __('Next Lesson', 'lifterlms'); ?>">
-				<?php _e( 'Next Lesson', 'yoast-academy' ); ?>
-			</a>
-		</div>
-
-	<?php } ?>
+	<?php if ( $next_lesson_id && $lesson->is_complete() ) : ?>
+		<a class="button next-lesson" href="<?php echo esc_url( get_permalink( $next_lesson_id ) ); ?>">
+			<?php _e( 'Next Lesson', 'yoastcom' ); ?>
+		</a>
+	<?php endif; ?>
 
 </nav>
 <div class="clear"></div>
