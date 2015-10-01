603d05e02af8772f7503fea1d2ad9279bd4b820a
Anton Timmermans <anton@yoast.com>
2015-10-01 13:45:36 +0200

    Add autostart option in the javascript

--- js/src/academy/quiz.js
+++ js/src/academy/quiz.js
@@ -5,6 +5,12 @@
 
 		// Open the quiz summary by default
 		$( '.view-summary' ).click();
+
+		// If the autostart hash is present, autostart the quiz
+		if ( '#start_quiz' === document.location.hash ) {
+			document.location.hash = '';
+			$( '#llms_start_quiz' ).click();
+		}
 	}
 
 	$( init );
