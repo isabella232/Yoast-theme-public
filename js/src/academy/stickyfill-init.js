2470af97d95cca5daf0b83677b1b3568e96e64e7
Anton Timmermans <anton@yoast.com>
2015-10-02 14:49:29 +0200

    Prevent weird whitespace on stickyfilled browsers

--- js/src/academy/stickyfill-init.js
+++ js/src/academy/stickyfill-init.js
@@ -2,6 +2,11 @@
 	$( function() {
 		$( '.yoast_academy .extra' ).each( function( i, item ) {
 			Stickyfill.add( item );
+
+			// If Sticky is natively supported it will make these functions identical.
+			if ( Stickyfill.add !== Stickyfill.remove ) {
+				$( 'body' ).addClass( 'sticky-filled' );
+			}
 		});
 	});
 }( jQuery ));
