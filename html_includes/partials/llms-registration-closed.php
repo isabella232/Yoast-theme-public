d11ce672339cd1c390c5af93c081f3b2fa838068
Anton Timmermans <anton@yoast.com>
2015-10-05 18:25:58 +0200

    Whitespace

--- html_includes/partials/llms-registration-closed.php
+++ html_includes/partials/llms-registration-closed.php
@@ -4,10 +4,9 @@ namespace Yoast\YoastCom\Theme;
 
 ?>
 <div class="col-2 llms-new-person-form-wrapper">
-<h2 class="llms-title llms-h2"><?php _e( 'New account', 'lifterlms' ); ?></h2>
+	<h2 class="llms-title llms-h2"><?php _e( 'New account', 'lifterlms' ); ?></h2>
 
-<?php _e( 'You can only register if you have bought a course.', 'yoastcom' ); ?>
+	<?php _e( 'You can only register if you have bought a course.', 'yoastcom' ); ?>
 
-<?php printf( __( 'Go to %1$s to buy a course', 'yoastcom' ), '<a href="https://yoast.com/courses/">yoast.com</a>' ); ?>
-
+	<?php printf( __( 'Go to %1$s to buy a course', 'yoastcom' ), '<a href="https://yoast.com/courses/">yoast.com</a>' ); ?>
 </div>
