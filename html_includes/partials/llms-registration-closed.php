f305542a8c043bb2c99c5ad76ed021b3634eaf2a
Anton Timmermans <anton@yoast.com>
2015-10-05 18:23:32 +0200

    Add "New account" header above yoast.com referral

--- html_includes/partials/llms-registration-closed.php
+++ html_includes/partials/llms-registration-closed.php
@@ -3,6 +3,8 @@
 namespace Yoast\YoastCom\Theme;
 
 ?>
+<h2 class="llms-title llms-h2"><?php _e( 'New account', 'lifterlms' ); ?></h2>
+
 <?php _e( 'You can only register if you have bought a course.', 'yoastcom' ); ?>
 
 <?php printf( __( 'Go to %1$s to buy a course', 'yoastcom' ), '<a href="https://yoast.com/courses/">yoast.com</a>' ); ?>
