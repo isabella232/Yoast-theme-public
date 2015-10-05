480cd486f2b321078887a2044d98f32545ee9107
Anton Timmermans <anton@yoast.com>
2015-10-05 18:25:13 +0200

    Add registration wrapper

--- html_includes/partials/llms-registration-closed.php
+++ html_includes/partials/llms-registration-closed.php
@@ -3,8 +3,11 @@
 namespace Yoast\YoastCom\Theme;
 
 ?>
+<div class="col-2 llms-new-person-form-wrapper">
 <h2 class="llms-title llms-h2"><?php _e( 'New account', 'lifterlms' ); ?></h2>
 
 <?php _e( 'You can only register if you have bought a course.', 'yoastcom' ); ?>
 
 <?php printf( __( 'Go to %1$s to buy a course', 'yoastcom' ), '<a href="https://yoast.com/courses/">yoast.com</a>' ); ?>
+
+</div>
