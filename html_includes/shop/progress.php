350d3fc9ec5343bcf120558d51de4755b62e88eb
Anton Timmermans <anton@yoast.com>
2015-10-06 16:03:15 +0200

    Add links to checkout progress

--- html_includes/shop/progress.php
+++ html_includes/shop/progress.php
@@ -7,10 +7,11 @@ namespace Yoast\YoastCom\Theme;
 
 $step = get_checkout_step();
 ?>
-<div class="progress">
+<div class="progress" id="steps-progress" data-step="<?php echo esc_attr( $step ); ?>">
 	<div class="row">
 		<ol class="list--steps">
 			<li>
+				<?php if ( 1 !== $step ) : ?><a href="<?php echo esc_url( url_shop_page() ); ?>"><?php endif; ?>
 				<?php
 					/* translators: Text between %1$s and %2$s is hidden on tablets, text between %3$s and %4$s is hidden on mobile. */
 					printf(
@@ -21,8 +22,10 @@ $step = get_checkout_step();
 						'</span>'
 					);
 				?>
+				<?php if ( 1 !== $step ) : ?></a><?php endif; ?>
 			</li>
 			<li<?php if ( 2 === $step ) : ?> class="step--current"<?php endif; ?>>
+				<?php if ( 2 !== $step ) : ?><a href="<?php echo esc_attr( url_checkout() ); ?>"><?php endif; ?>
 				<?php
 				/* translators: Text between %1$s and %2$s is hidden on large tablets, text between %3$s and %4$s is hidden on mobile. */
 					printf(
@@ -33,8 +36,10 @@ $step = get_checkout_step();
 						'</span>'
 					);
 				?>
+				<?php if ( 2 !== $step ) : ?></a><?php endif; ?>
 			</li>
 			<li<?php if ( 3 === $step ) : ?> class="step--current"<?php endif; ?>>
+				<?php if ( 3 !== $step ) : ?><a href="#" id="yst-to-step-3"><?php endif; ?>
 				<?php
 					/* translators: Text between %1$s and %2$s is hidden on large tablets, text between %3$s and %4$s is hidden on mobile. */
 					printf(
@@ -45,9 +50,12 @@ $step = get_checkout_step();
 						'</span>'
 					);
 				?>
+				<?php if ( 3 !== $step ) : ?></a><?php endif; ?>
 			</li>
 			<li<?php if ( 4 === $step ) : ?> class="step--current"<?php endif; ?>>
-				<span class="hide-on-mobile"><?php _e( 'Finished', 'yoastcom' ); ?></span>
+				<?php if ( 4 !== $step ) : ?><a href="#" id="yst-to-step-4"><?php endif; ?>
+					<span class="hide-on-mobile"><?php _e( 'Finished', 'yoastcom' ); ?></span>
+				<?php if ( 4 !== $step ) : ?></a><?php endif; ?>
 			</li>
 		</ol>
 	</div>
