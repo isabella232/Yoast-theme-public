6d33ecd1332c7eb12afe57e1967ceafca3a5142d
Anton Timmermans <anton@yoast.com>
2015-10-08 13:58:53 +0200

    Massively improve my-courses page styling

---
+++ html_includes/partials/llms-my-courses-course.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Yoast\YoastCom\Theme;
+
+/* @var \LLMS_Course $course */
+$course = $template_args['course'];
+
+$completed = ( 100 === (int) $course->get_percent_complete() );
+?>
+
+<li <?php post_class( array( 'my-courses-course' ), $course->id ); ?>>
+	<div class="img">
+		<?php echo lifterlms_get_featured_image( $course->id ); ?>
+	</div>
+
+	<div class="title">
+	<h3>
+		<a href="<?php echo esc_url( get_permalink( $course->id ) ); ?>">
+			<?php echo esc_html( get_the_title( $course->id ) ); ?>
+		</a>
+	</h3>
+	</div>
+	<?php if ( $completed ) : ?>
+		<div class="certificate">
+			<a class="button default" href="">
+				<i class="fa fa-graduation-cap" aria-hidden="true"></i><?php _e( 'Show certificate', 'yoastcom' ); ?>
+			</a>
+		</div>
+	<?php else : ?>
+		<div class="my-courses-progress">
+			<?php printf( __( 'Finished %d%%', 'yoastcom' ), $course->get_percent_complete() ); ?>
+		</div>
+
+		<div class="link">
+			<?php $next_lesson = $course->get_next_uncompleted_lesson(); ?>
+			<a class="button default" href="<?php echo esc_url( get_permalink( $next_lesson ) ); ?>">
+				<i class="fa fa-arrow-circle-right" aria-hidden="true"></i><?php printf( __( 'Continue %s', 'yoastcom' ), get_the_title( $course->id ) ); ?>
+			</a>
+		</div>
+	<?php endif; ?>
+</li>
