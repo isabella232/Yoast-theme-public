9723872ad1a6c5cb473786b3d42a724aadc883c7
Anton Timmermans <anton@yoast.com>
2015-10-23 11:11:12 +0200

    The header shouldn't be a h1

--- edd_templates/emails/body.php
+++ edd_templates/emails/body.php
@@ -3,7 +3,7 @@
        class="rounded6" style="border-radius: 6px;background-color: #ffffff;">
 	<tr>
 		<td align="left" valign="top" class="bodyContent" style="line-height: 200%;width:600px !important;font-family: Helvetica;font-size: 14px;color: #333333;padding: 20px !important;">
-			<h2 style="font-size: 22px;line-height: 28px;margin: 0 0 12px 0;"><?php echo EDD()->emails->get_heading(); ?></h2>
+			<h1 style="font-size: 22px;line-height: 28px;margin: 0 0 12px 0;"><?php echo EDD()->emails->get_heading(); ?></h1>
 
 			{email}
 		</td>
