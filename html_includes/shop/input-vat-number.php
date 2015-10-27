d52a4400d7b65308d6c22a4385b99827551a0d6e
Anton Timmermans <anton@yoast.com>
2015-10-27 08:34:31 +0100

    Change VAT number description
    Also change the location of the spinner because the new message messed
    up it's position.

--- html_includes/shop/input-vat-number.php
+++ html_includes/shop/input-vat-number.php
@@ -13,9 +13,9 @@ if ( isset( $_POST['yst_btw'] ) ) {
 <p id="yst-edd-btw-wrap">
 	<label for="yst_btw" class="edd-label">
 		<?php _e( 'VAT number (optional)', 'yoast-theme' ); ?>
+		<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>
 	</label>
-	<span class="edd-description"><?php _e( 'Please enter a valid VAT number to prevent paying VAT.', 'yoast-theme' ); ?></span>
+	<span class="edd-description"><?php _e( 'If you have a company with a valid VAT number, please enter it here, it will be checked automatically. If it checks out OK, you won&#8217;t have to pay VAT.', 'yoast-theme' ); ?></span>
 	<input type="text" id="yst_btw" name="yst_btw" class="edd-input ignore" placeholder="<?php _e( 'VAT no', 'yoast-theme' ); ?>" value="<?php echo esc_attr( $vat_nr ); ?>" />
-	<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>
 </p>
 <?php
