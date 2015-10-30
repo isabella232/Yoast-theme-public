fe36cfa9896b19b9b91c2d4df573e0cecb8c3471
Anton Timmermans <anton@yoast.com>
2015-10-30 15:22:05 +0100

    Add manage invites template and order invite email

---
+++ html_includes/emails/academy-order-invite.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Yoast\YoastCom\Theme;
+
+?>
+Thank you for buying multiple courses in Yoast Academy
+
+<a href="<?php echo esc_url( $template_args['url'] ); ?>">Login or register for an account here.</a> From there you can manage your course invites.
+
+If you cannot click on the link, paste this url in the browser: <?php echo esc_url( $template_args['url'] ); ?>
+
+
+Greetings,
+Team Yoast
+
