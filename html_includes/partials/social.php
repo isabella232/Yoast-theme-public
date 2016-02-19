0e341d265c70bcb6d774f627b2a11fbf689b1cac
Joost de Valk <joost@yoast.com>
2016-02-19 14:27:11 +0100

    Make social sharing counters settable from theme settings Move theme settings menu to Appearance menu

--- html_includes/partials/social.php
+++ html_includes/partials/social.php
@@ -13,15 +13,15 @@ if ( isset( $template_args['class'] ) ) {
 	</a>
 	<a class="fa fa-instagram" data-name="instagram" data-action="view" target="_blank" rel="publisher external" href="https://instagram.com/yoast">
 		<span>Instagram</span>
-		<span class="counter">1k</span>
+		<?php output_social_counter( 'instagram_followers' ); ?>
 	</a>
 	<a class="fa fa-linkedin-square" data-name="linkedin" data-action="view" target="_blank" rel="external" href="https://www.linkedin.com/company/yoast-com">
 		<span>LinkedIn</span>
-		<span class="counter">1k</span>
+		<?php output_social_counter( 'linkedin_followers' ); ?>
 	</a>
 	<a class="fa fa-pinterest-square" data-name="pinterest" data-action="view" target="_blank" rel="external" href="https://www.pinterest.com/yoast/">
 		<span>Pinterest</span>
-		<span class="counter">1k</span>
+		<?php output_social_counter( 'pinterest_followers' ); ?>
 	</a>
 	<a class="fa fa-twitter-square" data-name="twitter" data-action="view" target="_blank" rel="external" href="https://twitter.com/yoast">
 		<span>Twitter</span>
@@ -29,7 +29,7 @@ if ( isset( $template_args['class'] ) ) {
 	</a>
 	<a class="fa fa-youtube-square" data-name="youtube" data-action="view" target="_blank" rel="external" href="https://www.youtube.com/yoast">
 		<span>YouTube</span>
-		<span class="counter">2k</span>
+		<?php output_social_counter( 'youtube_subscribes' ); ?>
 	</a>
 	<a class="fa fa-rss-square" data-name="rss" data-action="view" target="_blank" rel="alternate" href="https://yoast.com/feed/">
 		<span>RSS Feed</span>
