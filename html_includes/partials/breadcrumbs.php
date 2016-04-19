51d9bb86fe346e2cce00ca4972d3169ddce727ab
Joost de Valk <joost@yoast.com>
2016-04-19 21:31:24 +0200

    remove small class from breadcrumbs

--- html_includes/partials/breadcrumbs.php
+++ html_includes/partials/breadcrumbs.php
@@ -2,7 +2,7 @@
 namespace Yoast\YoastCom\Theme;
 ?>
 <?php if ( function_exists( 'yoast_breadcrumb' ) ) : ?>
-<nav class="breadcrumb small">
+<nav class="breadcrumb">
 	<?php yoast_breadcrumb(); ?>
 </nav>
 <?php endif; ?>
