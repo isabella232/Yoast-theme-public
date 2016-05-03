caad829b586f12f0a5b95bfaa1233053f4bc8d64
Joost de Valk <joost@yoast.com>
2016-05-03 14:37:26 +0200

    Improvements for website review page + category lists

--- html_includes/partials/more-categories.php
+++ html_includes/partials/more-categories.php
@@ -7,9 +7,13 @@ $categories = query_categories( array( 'number' => 10 ) );
 	<a href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>" class="more more--category arrowed-medium <?php echo esc_attr( $template_args['class'] ); ?>">
 		<span class="more__plug show-on-desktop"><i class="fa fa-<?php echo get_term_meta( $category->term_id, 'yoastcom_term_icon', true ); ?>" aria-hidden="true"></i></span>
 		<div class="more__holder more__holder--naked more__holder--left">
-			<div class="more__supertitle"><?php echo esc_html( $category->name ); ?></div>
+			<div class="more__supertitle"><?php echo esc_html( $category->name ); ?>
 			<?php if ( $shortdesc = get_term_meta( $category->term_id, 'yoastcom_term_shortdesc', true ) ) : ?>
-			<div class="more__superlink hide-on-tablet"><?php echo $shortdesc; ?> &raquo;</div>
+				<i class="fa fa-angle-right"></i>
+			</div>
+			<div class="more__superlink color-black hide-on-tablet"><?php echo $shortdesc; ?> &raquo;</div>
+			<?php else: ?>
+			</div>
 			<?php endif; ?>
 		</div>
 	</a>
