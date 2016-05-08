35554d362d71f576052db474d9f4cb7522e5045c
Jip Moors <jhp.moors@gmail.com>
2016-05-08 13:07:20 +0200

    Restored title on tablet and below

--- html_includes/partials/post-title.php
+++ html_includes/partials/post-title.php
@@ -4,38 +4,44 @@ if ( has_post_thumbnail() ) {
 	$thumbnail = get_the_post_thumbnail();
 
 	?>
-	<div class="post-header">
-		<div class="post-header__image"><?php echo $thumbnail ?></div>
-		<div class="post-header__title">
-			<div class="post-header__title__container">
-				<h1><?php
+	<div class="hide-on-tablet">
+		<div class="post-header">
+			<div class="post-header__image"><?php echo $thumbnail ?></div>
+			<div class="post-header__title">
+				<div class="post-header__title__container">
+					<h1><?php
 
-					$custom_title_top = get_post_meta( get_the_ID(), 'title_top_line', true );
-					$custom_title_bottom = get_post_meta( get_the_ID(), 'title_bottom_line', true );
+						$custom_title_top    = get_post_meta( get_the_ID(), 'title_top_line', true );
+						$custom_title_bottom = get_post_meta( get_the_ID(), 'title_bottom_line', true );
 
-					if ( empty( $custom_title_top ) ) {
-						the_title();
-					}
-					else {
-						echo $custom_title_top;
-					}
+						if ( empty( $custom_title_top ) ) {
+							the_title();
+						}
+						else {
+							echo $custom_title_top;
+						}
 
-					if ( ! empty( $custom_title_bottom ) ) {
-						printf( '<br><span class="post-header__title--second-line">%s</span>', $custom_title_bottom );
-					}
-					
-					?></h1>
+						if ( ! empty( $custom_title_bottom ) ) {
+							printf( '<br><span class="post-header__title--second-line">%s</span>', $custom_title_bottom );
+						}
 
+						?></h1>
+
+				</div>
 			</div>
 		</div>
+		<div class="content__after-post-header"></div>
+		<div class="meta__after-post_header"></div>
 	</div>
-	<div class="content__after-post-header"></div>
-	<div class="meta__after-post_header"></div>
+
+	<div class="hide-on-desktop">
+		<h1><?php the_title(); ?></h1>
+	</div>
 	<?php
 }
 else {
-?>
-<h1><?php the_title(); ?></h1>
+	?>
+	<h1><?php the_title(); ?></h1>
 
-<?php
+	<?php
 }
