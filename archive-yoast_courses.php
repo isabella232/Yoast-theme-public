0c003aabdb8cf01d071ebc54824b4a1ed5002a6f
Joost de Valk <joost@yoast.com>
2016-06-06 12:57:38 +0200

    SEO courses page

--- archive-yoast_courses.php
+++ archive-yoast_courses.php
@@ -12,9 +12,21 @@ namespace Yoast\YoastCom\Theme;
 
 	<main role="main">
 		<div class="row">
-			<h1><?php _e( 'Courses by Yoast', 'yoastcom' ); ?></h1>
+			<h1>Online SEO Training by Yoast</h1>
+			<div class="content">
+				<p>Our online SEO courses will teach you about various aspects of SEO. We offer an online training
+					which covers all the basics of SEO. We also offer a course that focuses on our Yoast
+					SEO plugin, which teaches you everything you need to know to use our plugin to the
+					fullest. For everyone who creates content we have the SEO copywriting training. This SEO training
+					focuses on all aspects important in creating awesome and SEO-friendly articles.</p>
+			</div>
 		</div>
 
+		<hr>
+
+		<div class="row">
+			<h2>Available online SEO courses</h2>
+		</div>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<article class="row">
 				<?php get_template_part( 'html_includes/partials/course' ); ?>
@@ -24,6 +36,26 @@ namespace Yoast\YoastCom\Theme;
 				<hr>
 			<?php endif; ?>
 		<?php endwhile; ?>
+
+		<hr>
+
+		<div class="row">
+			<div class="content">
+				<h2>What every online SEO course contains</h2>
+				<p>All of our SEO courses contain lots of training videos in which Joost de Valk and other Yoast experts
+					explain the topic. Every SEO training challenges you to check whether or not you understood the
+					material. At the end of each lesson, you’ll have to fill out a quiz. All of our courses are
+					developed with the help of an educational specialist. For the SEO copywriting training, we worked
+					together with a linguist.</p>
+
+				<p>All of the Yoast courses contain high quality and state-of-the-art material. We update every course
+					at least annually, making sure we’ll teach you the latest insights. We give practical tips that’ll
+					really allow you to make improvements on any website!</p>
+
+				<p>Every course comes with a certificate for you to show your (prospective) customers that you know what
+					you're doing when it comes to SEO!</p>
+			</div>
+		</div>
 
 		<?php get_template_part( 'html_includes/partials/newsletter-subscribe' ); ?>
 
