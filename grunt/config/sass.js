7bd36b2bdff50b632b924ce5f0c4b2788c2acddf
Joost de Valk <joost@yoast.com>
2016-06-28 14:06:50 +0200

    Add SCSS source maps, remove unminified CSS files

--- grunt/config/sass.js
+++ grunt/config/sass.js
@@ -1,18 +1,8 @@
 // https://github.com/sindresorhus/grunt-sass
 module.exports = {
 	options: {
-		precision: 5
-	},
-	nested: {
-		options: {
-			outputStyle: 'nested'
-		},
-		files: {
-			'css/style.css': 'css/sass/style.scss',
-			'css/editor-style.css': 'css/sass/editor-style.scss',
-			'css/certificate.css': 'css/sass/certificate.scss',
-			'css/jquery-modal.css': 'css/sass/jquery.modal.scss'
-		}
+		precision: 5,
+		sourceMap: true
 	},
 	compressed: {
 		options: {
