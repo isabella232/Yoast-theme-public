0fb5ea15d3fdf446bb9b4a178c92631e3b2dd0e7
Diede Exterkate <diede@yoast.com>
2016-07-18 10:51:18 +0200

    escape variables

--- html_includes/partials/navigation-menu-link.php
+++ html_includes/partials/navigation-menu-link.php
@@ -6,16 +6,16 @@ namespace Yoast\YoastCom\Theme;
 $menu_item = $template_args['menu_item'];
 $type      = $template_args['type'];
 
-$anchor_class = ( ! empty( $menu_item['anchor_classes'] ) ? ' class="' . implode( ' ', $menu_item['anchor_classes'] ) . '"' : '' );
+$anchor_class = ( ! empty( $menu_item['anchor_classes'] ) ? ' class="' . implode( ' ', esc_attr( $menu_item['anchor_classes'] ) ) . '"' : '' );
 
-echo '<a href="' . $menu_item['url'] . '"' . $anchor_class . '>';
+echo '<a href="' . esc_url( $menu_item['url'] ) . '"' . $anchor_class . '>';
 
 if ( $type === 'main' ) {
 	echo esc_html( $menu_item['label'] );
 }
 
 if ( ! empty( $menu_item['icon'] ) ) {
-	echo '<span class="fa fa-' . $menu_item['icon'] . '" aria-hidden="true"></span>';
+	echo '<span class="fa fa-' . esc_html( $menu_item['icon'] ) . '" aria-hidden="true"></span>';
 }
 
 if ( $type === 'sub' ) {
