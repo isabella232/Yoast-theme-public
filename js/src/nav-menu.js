b480716efef7d677ac0b9319898e680f377df08e
Jip Moors <jhp.moors@gmail.com>
2016-07-19 16:18:37 +0200

    Remove current-menu-parent from active item. Applied variable name instead of hard-coded.

--- js/src/nav-menu.js
+++ js/src/nav-menu.js
@@ -5,8 +5,9 @@ jQuery( document ).ready( function( $ ) {
 	 */
 	function init() {
 		var parent_class = 'current-menu-parent';
-		var active_item = $( '.current-menu-parent' );
-		var non_active_items = $( '#yoast-main-menu .menu-item:not(.current-menu-parent)' );
+		var active_item = $( '.' + parent_class );
+		var all_items = $( '#yoast-main-menu .menu-item' );
+		var non_active_items = $( '#yoast-main-menu .menu-item:not(.' + parent_class + ')' );
 
 		non_active_items.hover(
 			function() {
@@ -21,10 +22,15 @@ jQuery( document ).ready( function( $ ) {
 		 * Set the clicked menu item as the new parent, unbind current event listeners and reinitialise the event listeners for the new 'current parent'.
 		 */
 		non_active_items.click( function( evt ) {
+			// Cleanup.
+			all_items.unbind( 'hover' );
+			all_items.unbind( 'click' );
+			all_items.removeClass( parent_class );
+
 			var clicked_menu_item = $( evt.target ).closest( '.menu-item' );
+			active_item = clicked_menu_item;
 			clicked_menu_item.addClass( parent_class );
-			non_active_items.unbind( 'hover' );
-			non_active_items.unbind( 'click' );
+
 			init();
 		} );
 	}
