18d9f279d730e9dc21f8fab1e1f06e2467613ead
Jip Moors <jhp.moors@gmail.com>
2016-07-22 11:17:49 +0200

    Used all_items as a base for sub-selection. Changed hover and click to bind statements to be unbinded. Remove acive_item assignment because unbinds are working properly now.

--- js/src/nav-menu.js
+++ js/src/nav-menu.js
@@ -7,9 +7,9 @@ jQuery( document ).ready( function( $ ) {
 		var parent_class = 'current-menu-parent';
 		var active_item = $( '.' + parent_class );
 		var all_items = $( '#yoast-main-menu .menu-item' );
-		var non_active_items = $( '#yoast-main-menu .menu-item:not(.' + parent_class + ')' );
+		var non_active_items = all_items.not( '.' + parent_class );
 
-		non_active_items.hover(
+		non_active_items.bind( 'hover',
 			function() {
 				active_item.removeClass( parent_class );
 			},
@@ -21,14 +21,13 @@ jQuery( document ).ready( function( $ ) {
 		/**
 		 * Set the clicked menu item as the new parent, unbind current event listeners and reinitialise the event listeners for the new 'current parent'.
 		 */
-		non_active_items.click( function( evt ) {
+		non_active_items.bind( 'click', function( evt ) {
 			// Cleanup.
 			all_items.unbind( 'hover' );
 			all_items.unbind( 'click' );
 			all_items.removeClass( parent_class );
 
 			var clicked_menu_item = $( evt.target ).closest( '.menu-item' );
-			active_item = clicked_menu_item;
 			clicked_menu_item.addClass( parent_class );
 
 			init();
