1f1c48c188996880629ab1116a01cf1dfbe8495f
Jip Moors <jhp.moors@gmail.com>
2016-09-14 14:55:33 +0200

    Change checkout step names

--- html_includes/shop/progress.php
+++ html_includes/shop/progress.php
@@ -29,7 +29,7 @@ $step = get_checkout_step();
 				<?php
 				/* translators: Text between %1$s and %2$s is hidden on tablets, text between %3$s and %4$s is hidden on mobile. */
 					printf(
-						'%1$sChoose your %2$s%3$spayment%4$s%1$s method%2$s',
+						'%1$sEnter %2$s%3$syour details%4$s',
 						'<span class="hide-on-tablet">',
 						'</span>',
 						'<span class="hide-on-mobile">',
@@ -39,23 +39,19 @@ $step = get_checkout_step();
 				<?php if ( 2 !== $step ) : ?></a><?php endif; ?>
 			</li>
 			<li<?php if ( 3 === $step ) : ?> class="step--current"<?php endif; ?>>
-				<?php if ( 3 !== $step ) : ?><a href="#" id="yst-to-step-3"><?php endif; ?>
 				<?php
 					/* translators: Text between %1$s and %2$s is hidden on tablets, text between %3$s and %4$s is hidden on mobile. */
 					printf(
-						'%1$sEnter payment %2$s%3$sdetails%4$s',
+						'%1$sChoose your %2$s%3$spayment method%4$s',
 						'<span class="hide-on-tablet">',
 						'</span>',
 						'<span class="hide-on-mobile">',
 						'</span>'
 					);
 				?>
-				<?php if ( 3 !== $step ) : ?></a><?php endif; ?>
 			</li>
 			<li<?php if ( 4 === $step ) : ?> class="step--current"<?php endif; ?>>
-				<?php if ( 4 !== $step ) : ?><a href="#" id="yst-to-step-4"><?php endif; ?>
-					<span class="hide-on-mobile"><?php _e( 'Finished', 'yoastcom' ); ?></span>
-				<?php if ( 4 !== $step ) : ?></a><?php endif; ?>
+				<span class="hide-on-mobile"><?php _e( 'Finished', 'yoastcom' ); ?></span>
 			</li>
 		</ol>
 	</div>
