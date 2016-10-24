64fd77d40a9ceb3532eeef5a2260bd54fb4eaae2
Jip Moors <jhp.moors@gmail.com>
2016-10-24 14:12:11 +0200

    Add translations to strings

--- html_includes/shop/switch-currency.php
+++ html_includes/shop/switch-currency.php
@@ -12,7 +12,7 @@ if ( is_null( $forced_currency ) ) {
 
 	$to_euros_class = $to_dollars_class = '';
 
-		if ( $template_args['current'] === 'USD' ) {
+	if ( $template_args['current'] === 'USD' ) {
 		$to_euros_class = ' hidden';
 	}
 
@@ -22,11 +22,11 @@ if ( is_null( $forced_currency ) ) {
 
 	// I want to pay in USD [switch to EUR]
 	// I want to pay in EUR [switch to USD]
-	$toggle = sprintf( __( 'I want to pay in %s', 'yoastcom' ), ' %1$s <button class="yst_currency_switch dimmed button--slim" data-currency="%2$s">switch to %2$s</button>' );
+	$toggle_template = sprintf( __( 'I want to pay in %s', 'yoastcom' ), ' %1$s <button class="yst_currency_switch dimmed button--slim" data-currency="%2$s">' . __( 'switch to', 'yoastcom' ) . ' %2$s</button>' );
 
 	echo '<p>';
-	echo '<span class="switch-currency switch-currency__USD' . $to_dollars_class . '">' . sprintf( $toggle, 'USD ($)', 'EUR' ) . '</span>';
-	echo '<span class="switch-currency switch-currency__EUR' . $to_euros_class . '">' . sprintf( $toggle, 'EUR (&euro;)', 'USD' ) . '</span>';
+	echo '<span class="switch-currency switch-currency__USD' . $to_dollars_class . '">' . sprintf( $toggle_template, 'USD ($)', 'EUR' ) . '</span>';
+	echo '<span class="switch-currency switch-currency__EUR' . $to_euros_class . '">' . sprintf( $toggle_template, 'EUR (&euro;)', 'USD' ) . '</span>';
 	echo '</p>';
 
 	echo '<div class="clear"></div>';
