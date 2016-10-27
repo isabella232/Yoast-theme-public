70f6a0a718dfc0d82d946684ac032a2d540b0747
Jip Moors <jhp.moors@gmail.com>
2016-10-27 11:22:22 +0200

    Contain search in "row"
    Remove blocks in the header
    Reduce the logo size
    Add more white space around logo
    Move search bar down on the homepage

--- html_includes/partials/search.php
+++ html_includes/partials/search.php
@@ -6,7 +6,7 @@ $search_url = apply_filters( 'yoastcom_search_url', home_url() );
 <div class="searchbar">
 	<form action="<?php echo esc_attr( $search_url ); ?>">
 		<input type="search" name="s"
-		       placeholder="<?php _e( apply_filters( 'yoast_theme-search_placeholder', 'Search' ), 'yoastcom' ); ?>">
+		       placeholder="<?php _e( apply_filters( 'yoast_theme-search_placeholder', 'Search&hellip;' ), 'yoastcom' ); ?>">
 		
 		<button class="button--naked search-button hide-on-desktop"><span class="visuallyhidden focusable"><?php _e( 'Search', 'yoastcom' ); ?></span><span class="text-icon">&#xf002;</span></button>
 	</form>
