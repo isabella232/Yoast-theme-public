9c23bbba618f59b73a57f16b6fdda31a285a3e61
Jip Moors <jhp.moors@gmail.com>
2016-11-28 11:03:26 +0100

    DRY bio social icons and add instragram

--- html_includes/partials/social-icons.php
+++ html_includes/partials/social-icons.php
@@ -1,30 +1,40 @@
 <?php
+
 namespace Yoast\YoastCom\Theme;
 
-if ( is_author() ) {
-	$twitter_url = get_the_author_meta( 'twitter' );
-	if ( ! empty( $twitter_url ) && strpos( $twitter_url, 'http' ) !== 0 ) {
-		$twitter_url = 'https://twitter.com/' . $twitter_url;
+$social_profiles = [
+	[
+		'title'      => __( 'Twitter', 'yoastcom' ),
+		'icon'       => '&#xf081;',
+		'identifier' => 'twitter',
+	],
+	[
+		'title'      => __( 'Linkedin', 'yoastcom' ),
+		'icon'       => '&#xf08c;',
+		'identifier' => 'linkedin',
+	],
+	[
+		'title'      => __( 'Facebook', 'yoastcom' ),
+		'icon'       => '&#xf082;',
+		'identifier' => 'facebook',
+	],
+	[
+		'title'      => __( 'Instagram', 'yoastcom' ),
+		'icon'       => '&#xf16d;',
+		'identifier' => 'instagram',
+	]
+];
+
+array_map( function ( $social_profile ) {
+
+	if ( author_has( 'twitter' ) ) {
+
+		printf( '<a href="%1$s" class="link--implicit"><span class="visuallyhidden focusable">%2$s</span><span class="text-icon text-icon--%3$s text-icon--social">%4$s</span></a>',
+			esc_url( url_social_site( $social_profile['identifier'] ) ),
+			$social_profile['title'],
+			$social_profile['identifier'],
+			$social_profile['icon']
+		);
 	}
-	$linkedin_url = get_the_author_meta( 'linkedin' );
-	$facebook_url = get_the_author_meta( 'facebook' );
-}
-?>
-<?php if ( ! empty( $twitter_url ) ) { ?>
-<a href="<?php echo esc_url( $twitter_url ) ; ?>" class="link--naked">
-	<span class="visuallyhidden focusable">Twitter</span>
-	<span class="text-icon text-icon--twitter text-icon--social">&#xf081;</span>
-</a>
-<?php } ?>
-<?php if ( ! empty( $linkedin_url ) ) { ?>
-<a href="<?php echo esc_url( $linkedin_url ) ; ?>" class="link--naked">
-	<span class="visuallyhidden focusable">Linkedin</span>
-	<span class="text-icon text-icon--linkedin text-icon--social">&#xf08c;</span>
-</a>	
-<?php } ?>
-<?php if ( ! empty( $facebook_url ) ) { ?>
-	<a href="<?php echo esc_url( $facebook_url ) ; ?>" class="link--naked">
-		<span class="visuallyhidden focusable">Facebook</span>
-		<span class="text-icon text-icon--facebook text-icon--social">&#xf082;</span>
-	</a>
-<?php } ?>
+
+}, $social_profiles );
