0c9dc813464ab55f12b733fec23fe25dcbb88e2a
Thijs Hulshof <thijsh@yoast.com>
2017-08-08 16:08:54 +0200

    Implemented requested copy changes

--- html_includes/partials/llms-registration-closed.php
+++ html_includes/partials/llms-registration-closed.php
@@ -6,10 +6,12 @@ $academy_overview = apply_filters( 'yoast:url', 'academy_overview');
 
 ?>
 <div class="col-2 llms-new-person-form-wrapper">
-	<h2 class="llms-title llms-h2"><?php _e( 'New account', 'lifterlms' ); ?></h2>
+	<h2 class="llms-title llms-h2"><?php _e( 'Add a new course', 'lifterlms' ); ?></h2>
 
 	<?php
-	_e( 'You can only register if you have bought a course.', 'yoastcom' );
-	printf( __( 'Go to %1$s to buy a course', 'yoastcom' ), '<a href="' . $academy_overview . 'courses/">yoast.com</a>' );
+	_e( '<p>Yoast Academy is only available once you have bought a course. Please visit our 
+	<a href="' . $academy_overview . 'courses/">Courses page</a> to see the available options. We have multiple in-depth courses, 
+	ranging from copywriting to site structure and technical SEO. Enhance your SEO skills with a Yoast course!</p>
+	<p><a href="' . $academy_overview . 'courses/">Check out all Yoast courses</a>.</p>' );
 	?>
 </div>
