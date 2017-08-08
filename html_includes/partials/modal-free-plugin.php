71e2b9d8197285a1fd80fa14acdd92f07fb780f5
Sjardo Janssen <sjardo@yoast.com>
2017-08-08 10:13:24 +0200

    Adds overlay for newsletter subscription when downloading the free plugin

---
+++ html_includes/partials/modal-free-plugin.php
@@ -0,0 +1,76 @@
+<?php
+function yst_footer_modal_script() {
+?>
+	<script>
+		jQuery(document).ready(function ($) {
+            $('a.free-plugin-download-modal').click(function (e) {
+                e.preventDefault(); // prevents the download
+                $('#free-plugin-download-modal').modal({
+                    closeText: '<i class="fa fa-times-circle"></i>'
+                });
+
+                position_modal();
+            });
+
+            $(window).resize(position_modal);
+
+            function position_modal(){
+                $windowheight = $(window).height();
+                $modal_margin_top = ($(window).height() / 2) - ($('#free-plugin-download-modal').outerHeight() / 2) + 'px';
+                $modal_margin_left = ($(window).width() / 2) - ($('#free-plugin-download-modal').outerWidth() / 2) + 'px';
+                $outerwidth = $('#free-plugin-download-modal').outerWidth() / 2;
+
+                $('#free-plugin-download-modal').css({
+                    'top':  $modal_margin_top,
+                    'left': $modal_margin_left,
+                    'position': 'absolute'
+                });
+            };
+        });
+	</script>
+<?php
+}
+
+wp_enqueue_script( 'jquery-modal' );
+add_action( 'wp_footer', 'yst_footer_modal_script', 90 );
+
+	?>
+	<div id="free-plugin-download-modal" style="display: none;">
+		<h3>
+			<?php _e( 'Great idea to get our free SEO plugin', 'yoastcom' ); ?>
+		</h3>
+		<div class="content content-downloading grid">
+            <div class="hourglass"><i class="fa fa-hourglass-end" aria-hidden="true"></i></div>
+            <div class="notification">
+                <h2><?php _e( 'Your plugin is downloading right now.', 'yoastcom' ); ?></h2>
+                <p><?php _e( 'You\'ll find it in your downloadfolder in a few seconds.', 'yoastcom' ); ?></p>
+            </div>
+
+
+		</div>
+        <div class="content content-usps">
+            <p>
+                <strong><?php _e( 'Wait! we get that you\'re thrilled to get started. but wait...', 'yoastcom' ); ?></strong><br/>
+                <?php _e( 'Why not first make sure you get the most out of it?', 'yoastcom' ); ?>
+            </p>
+            <ul class="list--usp">
+                <li><?php _e( 'Get weekly tips on how to optimize your website\'s SEO, usavility and conversion', 'yoastcom' ); ?></li>
+                <li><?php _e( 'Be the first to know about new features and other cool (free) plugins.', 'yoastcom' ); ?></li>
+                <li><?php _e( 'Get our <strong>free 50-page SEO guide</strong> right away to help you become an SEO genius.', 'yoastcom' ); ?></li>
+            </ul>
+            <form action="https://yoast.us1.list-manage.com/subscribe/post?u=ffa93edfe21752c921f860358&amp;id=972f1c9122" method="post" enctype="multipart/form-data">
+                <fieldset>
+                    <div class="grid">
+                        <div class="one-half">
+                            <label class="visuallyhidden" for="newsletter-email"><?php _e( 'Email', 'yoastcom' ); ?></label>
+                            <input type="email" placeholder="<?php _e( 'Enter your email address&hellip;', 'yoastcom' ); ?>" id="newsletter-email" name="EMAIL">
+                        </div>
+                        <div class="one-half">
+                            <button type="submit" class="default" name="Yes, give me your free tips"><?php _e( 'Yes, give me your free tips', 'yoastcom' ); ?></button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+
+        </div>
+	</div>
