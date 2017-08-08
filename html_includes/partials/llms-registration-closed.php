eb3683798b45db52c76bcbdd08194ecbcb672d26
Thijs Hulshof <thijsh@yoast.com>
2017-08-08 16:34:56 +0200

    Implemented new copy

--- html_includes/partials/llms-registration-closed.php
+++ html_includes/partials/llms-registration-closed.php
@@ -9,9 +9,16 @@ $academy_overview = apply_filters( 'yoast:url', 'academy_overview');
 	<h2 class="llms-title llms-h2"><?php _e( 'Add a new course', 'lifterlms' ); ?></h2>
 
 	<?php
-	_e( '<p>Yoast Academy is only available once you have bought a course. Please visit our 
-	<a href="' . $academy_overview . 'courses/">Courses page</a> to see the available options. We have multiple in-depth courses, 
-	ranging from copywriting to site structure and technical SEO. Enhance your SEO skills with a Yoast course!</p>
-	<p><a href="' . $academy_overview . 'courses/">Check out all Yoast courses</a>.</p>' );
+	_e( '<p>You can only access Yoast Academy after buying and activating one or more of our courses. In addition 
+    to our <a href="' . $academy_overview . 'course/yoast-seo-wordpress-training">Yoast SEO for WordPress plugin course</a>,
+    we offer a <a href="' . $academy_overview . 'course/basic-seo-training/">Basic SEO course</a>) and multiple 
+    in-depth courses on topics like SEO copywriting, site structure, and technical SEO. Please visit our
+    <a href="' . $academy_overview . 'courses/">Courses page</a> to find Yoast courses that suit your needs. 
+    Enhance your SEO skills with a Yoast course!</p>
+    <p>
+    Have you already bought a course? <a href="https://kb.yoast.com/kb/access-training-course/">This article</a> 
+    explains how to activate your courses and gain access to your Yoast Academy account.
+    </p>' );
 	?>
 </div>
+
