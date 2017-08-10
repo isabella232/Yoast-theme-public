fa259c488e8326664e65c5ef0c86cecb0e8ad044
Maarten Leenders <maarten@yoast.com>
2017-08-10 10:03:40 +0200

    Improved copy

--- html_includes/partials/llms-registration-closed.php
+++ html_includes/partials/llms-registration-closed.php
@@ -10,10 +10,10 @@ $academy_overview = apply_filters( 'yoast:url', 'academy_overview');
 
 	<?php
 	_e( '<p>You can only access Yoast Academy after buying and activating one or more of our courses. In addition 
-    to our <a href="' . $academy_overview . 'course/yoast-seo-wordpress-training">Yoast SEO for WordPress plugin course</a>,
-    we offer a <a href="' . $academy_overview . 'course/basic-seo-training/">Basic SEO course</a>) and multiple 
+    to our <a href="' . $academy_overview . 'course/yoast-seo-wordpress-training">Yoast SEO for WordPress course</a>,
+    we offer a <a href="' . $academy_overview . 'course/basic-seo-training/">basic SEO course</a> and multiple 
     in-depth courses on topics like SEO copywriting, site structure, and technical SEO. Please visit our
-    <a href="' . $academy_overview . 'courses/">Courses page</a> to find Yoast courses that suit your needs. 
+    <a href="' . $academy_overview . 'courses/">courses page</a> to find Yoast courses that suit your needs. 
     Enhance your SEO skills with a Yoast course!</p>
     <p>
     Have you already bought a course? <a href="https://kb.yoast.com/kb/access-training-course/">This article</a> 
@@ -21,4 +21,3 @@ $academy_overview = apply_filters( 'yoast:url', 'academy_overview');
     </p>' );
 	?>
 </div>
-
