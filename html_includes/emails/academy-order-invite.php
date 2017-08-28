14ece89143d60fefa3dc4f58159904e4b954d59e
Thijs Hulshof <thijsh@yoast.com>
2017-08-28 10:47:10 +0200

    processed final copy

--- html_includes/emails/academy-order-invite.php
+++ html_includes/emails/academy-order-invite.php
@@ -9,7 +9,7 @@ All that's left now is activating your course.
 <ol>
     <li><a href="<?php echo esc_url( $template_args['url'] ); ?>">Click this link.</a></li>
     <li>Login or create an account.</li>
-    <li>Activate your course by clicking the button.</li>
+    <li>Activate your course by clicking the button 'Add <?php echo $template_args['title']; ?> to this account'.</li>
     <li>Your course is now available in <a href="https://yoast.academy/my-courses/">My Academy</a>.</li>
 </ol>
 
