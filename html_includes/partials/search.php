05977a9fdd2c80d71c2a98f91431f9da962cc247
Sjardo Janssen <sjardo@yoast.com>
2017-08-31 15:13:54 +0200

    fixes indentation and reverted "search" back to "s"

--- html_includes/partials/search.php
+++ html_includes/partials/search.php
@@ -8,7 +8,7 @@ $search_url = apply_filters( 'yoastcom_search_url', home_url() );
         <span class="fa fa-search" aria-label=""></span>
     </div>
 	<form action="<?php echo esc_attr( $search_url ); ?>">
-		<input type="search" name="search"
+		<input type="search" name="s"
 		       placeholder="<?php _e( apply_filters( 'yoast_theme-search_placeholder', 'Search&hellip;' ), 'yoastcom' ); ?>">
 		<button class="button--naked search-button hide-on-desktop"><span class="visuallyhidden focusable"><?php _e( 'Search', 'yoastcom' ); ?></span><span class="fa fa-search" aria-label=""></span></button>
 	</form>
