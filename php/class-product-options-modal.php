2ad3a44258246bec1f59e44dce0b9db1df895bdc
Diede Exterkate <diede@yoast.com>
2017-09-05 16:04:09 +0200

    prepare model data dynamically

--- php/class-product-options-modal.php
+++ php/class-product-options-modal.php
@@ -17,7 +17,7 @@ class Product_Options_Modal {
 			}
 		}
 
-		self::$modals[] = $args;
+		self::$modals[] = self::build_args( $args );
 
 		$add_modal_to_footer = array( __CLASS__, 'buy_button_modal' );
 		if ( ! has_action( 'wp_footer', $add_modal_to_footer ) ) {
@@ -26,32 +26,48 @@ class Product_Options_Modal {
 	}
 
 	public static function buy_button_modal() {
-		add_action( 'wp_footer', array( __CLASS__, 'footer_modal_script'), 90 );
+		add_action( 'wp_footer', array( __CLASS__, 'footer_modal_script' ), 90 );
 		get_template_part( '/html_includes/partials/modal', self::$modals );
 		wp_enqueue_script( 'jquery-modal' );
 	}
 
 	public static function footer_modal_script() {
 		?>
-<script>
-	jQuery( document ).ready( function ( $ ) {
-		$( 'a[data-modal-product-id]' ).click( function ( e ) {
-			e.preventDefault();
+        <script>
+            jQuery(document).ready(function ($) {
+                $('a[data-modal-product-id]').click(function (e) {
+                    e.preventDefault();
 
-			var $this = $(this);
-			var product_id = $this.data('modal-product-id');
+                    var $this = $(this);
+                    var product_id = $this.data('modal-product-id');
 
-			$( '#prices-modal-' + product_id ).modal( {
-				closeText: '<i class="fa fa-times-circle"></i>'
-			} );
-		} );
-		$( '.modal label' ).click( function ( e ) {
-			e.preventDefault();
-			$( 'input#' + $( this ).attr( 'for' ) ).prop( 'checked', true );
-		} );
-	} );
-</script>
+                    $('#prices-modal-' + product_id).modal({
+                        closeText: '<i class="fa fa-times-circle"></i>'
+                    });
+                });
+                $('.modal label').click(function (e) {
+                    e.preventDefault();
+                    $('input#' + $(this).attr('for')).prop('checked', true);
+                });
+            });
+        </script>
 		<?php
+	}
+
+	public static function build_args( $args ) {
+		$product_id                = $args['id'];
+		$dynamic_pricing           = apply_filters( 'dynamic_pricing_product_rules', \WC_Dynamic_Pricing_Compatibility::get_product_meta( wc_get_product( $product_id ), '_pricing_rules' ) );
+		$args['dynamic_pricing'][] = [
+			'from'   => '1',
+			'to'     => '1',
+			'amount' => '0',
+			'type'   => 'percentage_discount',
+
+		];
+		$args['dynamic_pricing']   = array_merge( $args['dynamic_pricing'], current( $dynamic_pricing )['rules'] );
+
+
+		return $args;
 	}
 
 
